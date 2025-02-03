334d24998363926a96936829600abc323de50d6c
SillyCookiemonster <meyere3109@masseyhigh.school.nz>
2025-02-03 13:25:36 +1300

    Yes or No Checker

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -3,8 +3,14 @@ while True:
 
     if want_instructions in ['y', 'yes']:
         print("You chose yes.")
+        break
 
     elif want_instructions in ['n', 'no']:
         print("You chose no.")
+        break
+
     else:
         print("Please choose yes or no.")
+        continue
+
+print("we done")
